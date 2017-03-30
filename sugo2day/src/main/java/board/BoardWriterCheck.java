6ad67cdbb43d4f0fdd6f42c1e0801fe6d6c60d0d
ds <ds@ds601-06>
2017-03-30 14:51:22 +0900

    170330 14:51 / JJH

---
+++ sugo2day/src/main/java/board/BoardWriterCheck.java
@@ -0,0 +1,33 @@
+package board;
+
+import bean.BoardBean;
+import dao.BoardDao;
+import exception.BoardNotFoundException;
+
+public class BoardWriterCheck {
+	String writer;
+	BoardDao boardDao;
+	
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+	public void setBoardDao(BoardDao boardDao) {
+		this.boardDao = boardDao;
+	}
+
+	public void match(int num, String writer) {
+		int result = boardDao.checkWriter(num, writer);
+		if(result == 0) {
+			throw new BoardNotFoundException();  
+		} else if(result == 1) {
+			boardDao.delete(num, writer);
+		}
+		
+	}
+
+}
