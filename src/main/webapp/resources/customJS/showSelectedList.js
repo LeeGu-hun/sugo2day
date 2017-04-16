efc5557b0901c7a92e61c45f5c64afa5c1bc5eba
Administrator <Administrator@192.168.35.242>
2017-04-16 09:21:23 +0900

    555555

--- src/main/webapp/resources/customJS/showSelectedList.js
+++ src/main/webapp/resources/customJS/showSelectedList.js
@@ -50,18 +50,19 @@ function showSelectedList(selItem) {
 	    		html += '<h4 class="lead">' + jsonResult[i].regdate + '</h4>';
 	    		html += '</div>';
 	    		html += '</div>';
-	    		html += '<c:if test="${' + jsonResult[i].files + ' == null}">';
-	    		html += '<div style="max-width: 600px;">';
-	    		html += '<c:set var="ext" value="${fn:split(' + jsonResult[i].files + ',".")}"/>';
-	    		html += '<c:choose>';
-	    		html += '<c:when test="${ext[1] eq "jpg" || ext[1] eq "gif" || ext[1] eq "png"}">';
-	    		html += "<img class='img-responsive' src='http://localhost:8080/sugo2day/uploads/" + jsonResult[i].files + "'>";
-	    		html += '</c:when>';
-	    		html += '<c:otherwise>첨부된 사진 없음</c:otherwise>';
-	    		html += '</c:choose>';
-	    		html += '</div>';
-	    		html += '</c:if>';
+	    		if(!isEmpty(jsonResult[i].files)) {
+		    		html += '<div style="max-width: 600px;">';
+		    		html += "<img class='img-responsive' src='http://localhost:8080/sugo2day/uploads/" + jsonResult[i].files + "'>";
+		    		html += '</div>';
+	    		} else if (isEmpty(jsonResult[i].files)) {
+	    			html += '<div style="max-width: 600px;">';
+	    			html += '<h5>등록된 사진이 없거나 잘못된 포맷입니다.</h5>';
+	    			html += '<h5>허용되는 사진 포맷은 jpg, gif, png 입니다.</h5>'
+	    			html += '</div>';
+	    		}
+	    		
 	    		html += '<br>';
+	    		
 	    		html += '<div style="max-width: 600px;" class="list_content">';
 	    		html += '<textarea class="lead" id="content" cols="52" rows="6" style="resize: none; overflow-y:scroll"';
 	    		html += 'readonly="readonly">' + jsonResult[i].content + '</textarea>';
@@ -71,11 +72,11 @@ function showSelectedList(selItem) {
 	    		html += '});';
 	    		html += '</script>';
 	    		html += '</div>';
-	    		html += '<c:if test="! empty ' + jsonResult[i].startdate + '">';
+	    		
 	    		html += '<div class="list_date">';
 	    		html += '<h4 class="lead"> ' + jsonResult[i].startdate + '  부터 ' + jsonResult[i].enddate + '  까지</h4>';
 	    		html += '</div>	';
-	    		html += '</c:if>';
+	    		
 	    		html += '<div class="pull-left">';
 	    		html += '<div class="user_id">';
 	    		html += '<h3 class="lead">by <b> ' + jsonResult[i].writer + ' </b></h3>';
@@ -89,9 +90,7 @@ function showSelectedList(selItem) {
 	    	}
 	    	html += '</div>';
 	    	$('#qselected-list').html(html);   
-    	} 
-    	
-    	if (isEmpty(jsonResult)) {
+    	} else if (isEmpty(jsonResult)) {
     		var noQuest = '<h3>선택한 퀘스트 카테고리로 작성한 글이 없습니다.</h3>';
     		$('#qselected-list').html(noQuest);
     	}
